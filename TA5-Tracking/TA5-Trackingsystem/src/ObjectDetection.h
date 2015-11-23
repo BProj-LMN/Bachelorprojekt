60d64f01f3a6526b5a2277d74d7ecf479aba71a5
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2015-11-23 21:33:32 +0100

    [CODE] objectDetection erster Entwurf

--- TA5-Tracking/TA5-Trackingsystem/src/ObjectDetection.h
+++ TA5-Tracking/TA5-Trackingsystem/src/ObjectDetection.h
@@ -1,23 +1,35 @@
 /*
  * objectDetection.h
  *
- *  Created on: 12.11.2015
- *      Author: Jannik
+ * function: detect object in an image
+ *
+ * author: Jannik Beyerstedt
  */
 
 #ifndef SRC_OBJECTDETECTION_H_
 #define SRC_OBJECTDETECTION_H_
 
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+using namespace cv;
+
 #include "Camera.h"
+#include "globalConstants.h"
 
 class ObjectDetection {
   Camera* cam;
+  Mat refereceFrame;
 
 public:
   ObjectDetection(Camera* cam);
   virtual ~ObjectDetection();
   
-  void detectObject(Mat frame, vector<float>* pixelPosition);
+  void setReferenceFrame(Mat frame);
+  void detectObject(Mat frame, int pixelPosition[2]);
+
+private:
+  int getObjectPosition(Mat thresholdImage, int objectPosition[2], Rect* boundingRectangle);
 };
 
 #endif /* SRC_OBJECTDETECTION_H_ */
