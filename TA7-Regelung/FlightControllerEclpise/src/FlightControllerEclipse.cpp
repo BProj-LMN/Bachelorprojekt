0ad5478e3fae9bcf4ffc9d7051b0ee7ac0edfce1
Marcel <marcel.schmid@haw-hamburg.de>
2015-12-11 13:04:54 +0100

    [CODE] Trajec in Trajektorie umbenannt, hübsch machen

--- TA7-Regelung/FlightControllerEclpise/src/FlightControllerEclipse.cpp
+++ TA7-Regelung/FlightControllerEclpise/src/FlightControllerEclipse.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : FlightControllerEclipse.cpp
-// Author      : Kai Robin Möller
+// Author      : Kai Robin Mï¿½ller
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -28,28 +28,28 @@ double istX, istY, istZ;
 int landen = 0;
 int Error = 0;
 
-//Regler für Vor Zurück erzeugen Grenzwerte für Regelung von -127 bis 127
+//Regler fÃ¼r Vor ZurÃ¼ck erzeugen Grenzwerte fÃ¼r Regelung von -127 bis 127
 PID_Regler reglerX = PID_Regler(-REGELMAX / 2, REGELMAX / 2);
-//Regler für Rechts Links erzeugen Grenzwerte für Regelung von -127 bis 127
+//Regler fÃ¼r Rechts Links erzeugen Grenzwerte fÃ¼r Regelung von -127 bis 127
 PID_Regler reglerY = PID_Regler(-REGELMAX / 2, REGELMAX / 2);
-//Regler für Hoch Runter erzeugen Grenzwerte für Regelung von 0 bis 254
+//Regler fÃ¼r Hoch Runter erzeugen Grenzwerte fÃ¼r Regelung von 0 bis 254
 PID_Regler reglerZ = PID_Regler(0, REGELMAX);
 //Trajektorien Klasse erzeugen
-Trajectory_Class trajec = Trajectory_Class();
+Trajectory_Class Trajectory = Trajectory_Class();
 //UserInterface erzeugen
 UserInterface UI = UserInterface(&Error);
 //Serielle Schnittstelle erzeugen
 Wrapper_Steuerung Steuerung = Wrapper_Steuerung();
 
 void Sollwertvorgabe() {
-    //nächsten Checkpoint zuweisen
-    reglerX.setSoll(trajec.getNextCheckpointX());
-    reglerY.setSoll(trajec.getNextCheckpointY());
-    reglerZ.setSoll(trajec.getNextCheckpointZ());
+    //nÃ¤chsten Checkpoint zuweisen
+    reglerX.setSoll(Trajectory.getNextCheckpointX());
+    reglerY.setSoll(Trajectory.getNextCheckpointY());
+    reglerZ.setSoll(Trajectory.getNextCheckpointZ());
 }
 
 void Startprozedur() {
-    //Um eine Verbindung mit dem Copter aufzubauen muss der Schub einmal auf Maximalausschlag und wieder zurück
+    //Um eine Verbindung mit dem Copter aufzubauen muss der Schub einmal auf Maximalausschlag und wieder zurÃ¼ck
     cout << "Mit dem Copter Verbindung aufbauen" << endl;
     Steuerung.HochRunter(REGELMAX);
     Steuerung.Steuern();
@@ -70,7 +70,7 @@ int main(int argc, char** argv) {
     //Istwerte einlesen
 
     //Trajectorie Checkpoints berechnen
-    trajec.calcCheckpointsA(istX, istY, istZ, UI.getX(), UI.getY(), UI.getZ()); // Checkpoints berrechnen
+    Trajectory.calcCheckpointsB(istX, istY, istZ, UI.getX(), UI.getY(), UI.getZ()); // Checkpoints berechnen
     cout << "Checkpoints berechnet" << endl;
     // erster checkpoint als soll vorgeben
     Sollwertvorgabe();
@@ -85,7 +85,7 @@ int main(int argc, char** argv) {
     }
     //Warten auf Enter
     cout << "Copter Regelung ist gestartet" << endl;
-    cout << "Um den Copter landen zulassen Leertaste betaetigen" << endl;
+    cout << "Um den Copter landen zu lassen Leertaste betaetigen" << endl;
     cout << "oder Enter betaetigen um neue Sollwerte zu definieren" << endl;
     while ((!UI.LeertasteGedrueckt())&&(Error == 0)) {
         if (UI.LeertasteGedrueckt()) {
@@ -94,19 +94,19 @@ int main(int argc, char** argv) {
         //Istwerte einlesen
 
         //wenn der aktuelle Checkpoint erreicht ist
-        if (trajec.checkpointReached(istX, istY, istZ)) {
-            trajec.nextCheckpoint(); // nächsten Checkpoint auswählen
-            Sollwertvorgabe(); //neuen Checkpoint dem Regler übergeben
+        if (Trajectory.checkpointReached(istX, istY, istZ)) {
+            Trajectory.nextCheckpoint(); // nÃ¤chsten Checkpoint auswÃ¤hlen
+            Sollwertvorgabe(); //neuen Checkpoint dem Regler Ã¼bergeben
             cout << "Checkpoint wurde erreicht" << endl;
         } else;
         //Regeln
-        //Übergibt die Regelwerte an den puffer der Seriellen Schnittstelle
+        //Ã¼bergibt die Regelwerte an den puffer der Seriellen Schnittstelle
         Steuerung.HochRunter((int) reglerZ.getControlValue(istZ));
         Steuerung.RechtLinks((int) reglerY.getControlValue(istY));
         Steuerung.VorZurueck((int) reglerX.getControlValue(istX));
         //Regelwerte an die Fernsteuerung senden
         Steuerung.Steuern();
-        //Wenn Enter betätigt wird können neue Sollwerte definiert werden dabei soll der Copter die aktuelle position halten
+        //Wenn Enter betÃ¤tigt wird kÃ¶nnen neue Sollwerte definiert werden dabei soll der Copter die aktuelle position halten
         if (UI.EnterGedrueckt()) {
             //Istwerte einlesen
 
@@ -120,7 +120,7 @@ int main(int argc, char** argv) {
             while (!UI.sollEinlesen()&&(Error == 0)) {
                 //Istwerte einlesen
 
-                //Übergibt die Regelwerte an den puffer der Seriellen Schnittstelle
+                //Ã¼bergibt die Regelwerte an den puffer der Seriellen Schnittstelle
                 Steuerung.HochRunter((int) reglerZ.getControlValue(istZ));
                 Steuerung.RechtLinks((int) reglerY.getControlValue(istY));
                 Steuerung.VorZurueck((int) reglerX.getControlValue(istX));
@@ -128,14 +128,14 @@ int main(int argc, char** argv) {
                 Steuerung.Steuern();
             }
             //neue Trajektorie wird berrechnet
-            trajec.calcCheckpointsA(istX, istY, istZ, UI.getX(), UI.getY(), UI.getZ());
+            Trajectory.calcCheckpointsA(istX, istY, istZ, UI.getX(), UI.getY(), UI.getZ());
             cout << "Checkpoints wurden berechnet" << endl;
             cout << "Um Copter weiter fliegen zu lassen Enter betaetigen" << endl;
-            //Wartet bis Enter betätigt wird bevor der Copter wieder losfliegt
+            //Wartet bis Enter betÃ¤tigt wird bevor der Copter wieder losfliegt
             while (!UI.EnterGedrueckt()&&(Error == 0)) {
                 //Istwerte einlesen
 
-                //Übergibt die Regelwerte an den puffer der Seriellen Schnittstelle
+                //Ã¼bergibt die Regelwerte an den puffer der Seriellen Schnittstelle
                 Steuerung.HochRunter((int) reglerZ.getControlValue(istZ));
                 Steuerung.RechtLinks((int) reglerY.getControlValue(istY));
                 Steuerung.VorZurueck((int) reglerX.getControlValue(istX));
@@ -143,7 +143,7 @@ int main(int argc, char** argv) {
                 Steuerung.Steuern();
             }
         }
-        //Um Copter landen zu lassen Leertaste betätigen
+        //Um Copter landen zu lassen Leertaste betï¿½tigen
     }
     if (Steuerung.HochAktuell()!=0) {
         //Landenprozedur
