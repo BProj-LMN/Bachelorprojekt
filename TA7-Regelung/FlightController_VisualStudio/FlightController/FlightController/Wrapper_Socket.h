f4a9dcae93ba4448e85809ff22d5e3e5c7b6c906
RobinMoeller <kairobin.moeller@haw-hamburg.de>
2015-12-17 14:11:02 +0100

    [CODE]Marcels kram rumgeschmissen

---
+++ TA7-Regelung/FlightController_VisualStudio/FlightController/FlightController/Wrapper_Socket.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Wrapper_Socket.h
+ * Author: Robin
+ *
+ * Created on 10. Dezember 2015, 10:21
+ */
+
+#ifndef WRAPPER_SOCKET_H
+#define	WRAPPER_SOCKET_H
+
+class Wrapper_Socket{
+public:
+    
+private:
+    
+};
+
+
+#endif	/* WRAPPER_SOCKET_H */
+
