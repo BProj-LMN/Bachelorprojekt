7c6e26ef46fd67476ee010abe7fc32aeb6beccd6
Martin <martin.kroh@haw-hamburg.de>
2016-01-05 10:21:28 +0100

    [CODE] triangulate angepasst

--- TA5-Tracking/TA5-1-Lotfußpunktverfahren/src/triangulate.h
+++ TA5-Tracking/TA5-1-Lotfußpunktverfahren/src/triangulate.h
@@ -13,122 +13,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <cstdlib>
 #define MYEPS 1.0E-19
 #define MAX 2
 
-/*** Beispiel 1***/
-/*
-float K1_vec[] = {1, 6, 4};
-float u_vec[] = {1, 2, 1};
-float K2_vec[] = {4, -4, 3};
-float v_vec[] = {-1, 2, 3};
- */
+void triangulate(Point3f& cam1_p, Point3f& cam1_u, Point3f& cam2_p, Point3f& cam2_u, Point3f& position) {
 
-/*** Beispiel 2***/
-/*
-float K1_vec[] = {-7, 2, -3};
-float u_vec[] = {0, 1, 2};
-float K2_vec[] = {-3, -3, 3};
-float v_vec[] = {1, 2, 1};
-*/
+  /*** Interne Variablen zur Ermittlung des Verbindungsvektors und
+   der Elemente des linearen Gleichungssystems ***/
+  float K1_vec[];
+  float u_vec[];
+  float K2_vec[];
+  float v_vec[];
 
-float triangulate(float *K1_vec, float *u_vec, float *K2_vec, float *v_vec) {
+  K1_vec[0] = cam1_p.x;
+  K1_vec[1] = cam1_p.y;
+  K1_vec[2] = cam1_p.z;
+  u_vec[0] = cam1_u.x;
+  u_vec[1] = cam1_u.y;
+  u_vec[2] = cam1_u.z;
+  K2_vec[0] = cam2_p.x;
+  K2_vec[1] = cam2_p.y;
+  K2_vec[2] = cam2_p.z;
+  v_vec[0] = cam2_u.x;
+  v_vec[1] = cam2_u.y;
+  v_vec[2] = cam2_u.z;
 
-    /*** Interne Variablen zur Ermittlung des Verbindungsvektors und
-         der Elemente des linearen Gleichungssystems ***/
-    int i = 0;
-    int r = 0;
-    int h = 0;
-    float a[MAX + 1][MAX + 1];
-    float b[MAX + 1][MAX + 1];
-    float x[MAX][MAX];
-    float y[MAX];
-    float Skalar[MAX];
-    float Verbindungsvektor[MAX + 1];
-    float Verbindungsvektor_halbiert[MAX + 1];
-    float Schnittpunkt[MAX + 1];
+  int i = 0;
+  int r = 0;
+  int h = 0;
+  float a[MAX + 1][MAX + 1];
+  float b[MAX + 1][MAX + 1];
+  float x[MAX][MAX];
+  float y[MAX];
+  float Skalar[MAX];
+  float Verbindungsvektor[MAX + 1];
+  float Verbindungsvektor_halbiert[MAX + 1];
+  float Schnittpunkt[MAX + 1];
 
-    /*** Interne Variablen zur Lösung des LGS ***/
-    int S0 = 0;
-    int S2 = 0;
-    int S3 = 0;
-    float q;
-    float Pivotelement = 0.0;
-    int S4 = 0;
-    int S5 = 0;
-    float Summe;
+  /*** Interne Variablen zur Lösung des LGS ***/
+  int S0 = 0;
+  int S2 = 0;
+  int S3 = 0;
+  float q;
+  float Pivotelement = 0.0;
+  int S4 = 0;
+  int S5 = 0;
+  float Summe;
 
-    /*** Ermittlung des LGS aus Orthogonalitätsbedingungen ***/
-    for (i = 0; i <= MAX; i++) {
-        a[i][0] = (K2_vec[i] - K1_vec[i]) * u_vec[i];
-        a[i][1] = v_vec[i] * u_vec[i];
-        a[i][2] = (-1) * u_vec[i] * u_vec[i];
-        b[i][0] = (K2_vec[i] - K1_vec[i]) * v_vec[i];
-        b[i][1] = v_vec[i] * v_vec[i];
-        b[i][2] = (-1) * u_vec[i] * v_vec[i];
+  /*** Ermittlung des LGS aus Orthogonalitätsbedingungen ***/
+  for (i = 0; i <= MAX; i++) {
+    a[i][0] = (K2_vec[i] - K1_vec[i]) * u_vec[i];
+    a[i][1] = v_vec[i] * u_vec[i];
+    a[i][2] = (-1) * u_vec[i] * u_vec[i];
+    b[i][0] = (K2_vec[i] - K1_vec[i]) * v_vec[i];
+    b[i][1] = v_vec[i] * v_vec[i];
+    b[i][2] = (-1) * u_vec[i] * v_vec[i];
+  }
+
+  /*** 2x2-Matrix und 2x1-Vektor für LGS ***/
+  x[0][0] = a[0][1] + a[1][1] + a[2][1];
+  x[0][1] = a[0][2] + a[1][2] + a[2][2];
+  x[1][0] = b[0][1] + b[1][1] + b[2][1];
+  x[1][1] = b[0][2] + b[1][2] + b[2][2];
+  y[0] = -(a[0][0] + a[1][0] + a[2][0]);
+  y[1] = -(b[0][0] + b[1][0] + b[2][0]);
+
+  /*** Vorwärts eliminieren ***/
+  for (S0 = 0; S0 < (MAX - 1); S0++) {
+    Pivotelement = x[S0][S0];
+    for (S2 = 1; S2 < (MAX - S0); S2++) {
+      q = ((x[(S2 + S0)][S0] / Pivotelement) * (-1));
+      for (S3 = 0; S3 < (MAX - S0); S3++) {
+        x[(S0 + S2)][(S0 + S3)] = ((x[S0][(S0 + S3)] * q) + x[S0 + S2][(S0 + S3)]);
+      };
+      y[S0 + S2] = ((y[S0] * q) + y[S0 + S2]);
     }
+  }
 
-    /*** 2x2-Matrix und 2x1-Vektor für LGS ***/
-    x[0][0] = a[0][1] + a[1][1] + a[2][1];
-    x[0][1] = a[0][2] + a[1][2] + a[2][2];
-    x[1][0] = b[0][1] + b[1][1] + b[2][1];
-    x[1][1] = b[0][2] + b[1][2] + b[2][2];
-    y[0] = -(a[0][0] + a[1][0] + a[2][0]);
-    y[1] = -(b[0][0] + b[1][0] + b[2][0]);
-
-    /*** Vorwärts eliminieren ***/
-    for (S0 = 0; S0 < (MAX - 1); S0++) {
-        Pivotelement = x[S0][S0];
-        for (S2 = 1; S2 < (MAX - S0); S2++) {
-            q = ((x[(S2 + S0)][S0] / Pivotelement) * (-1));
-            for (S3 = 0; S3 < (MAX - S0); S3++) {
-                x[(S0 + S2)][(S0 + S3)] = ((x[S0][(S0 + S3)] * q) + x[S0 + S2][(S0 + S3)]);
-            };
-            y[S0 + S2] = ((y[S0] * q) + y[S0 + S2]);
-        }
+  /*** Rückwärts einsetzten ***/
+  for (S4 = (MAX - 1); S4 >= 0; S4--) {
+    Summe = 0;
+    for (S5 = (S4 + 1); S5 <= (MAX - 1); S5++) {
+      Summe = Summe + (x[S4][S5]) * Skalar[S5];
     }
+    Skalar[S4] = ((y[S4]) - Summe) / x[S4][S4];
+  }
 
-    /*** Rückwärts einsetzten ***/
-    for (S4 = (MAX - 1); S4 >= 0; S4--) {
-        Summe = 0;
-        for (S5 = (S4 + 1); S5 <= (MAX - 1); S5++) {
-            Summe = Summe + (x[S4][S5]) * Skalar[S5];
-        }
-        Skalar[S4] = ((y[S4]) - Summe) / x[S4][S4];
-    }
+  /*** Berechnung des Verbindungsvektors ***/
+  for (i = 0; i <= MAX; i++) {
+    Verbindungsvektor[i] = (K2_vec[i] - K1_vec[i]) + v_vec[i] * Skalar[0] - u_vec[i] * Skalar[1];
+  }
 
-    /*** Berechnung des Verbindungsvektors ***/
-    for (i = 0; i <= MAX; i++) {
-        Verbindungsvektor[i] = (K2_vec[i] - K1_vec[i]) + v_vec[i] * Skalar[0] - u_vec[i] * Skalar[1];
-    }
+  /*** Halbierung des Verbindungsvektors ***/
+  for (i = 0; i <= MAX; i++) {
+    Verbindungsvektor_halbiert[i] = 0.5 * Verbindungsvektor[i];
+  }
 
-    /*** Halbierung des Verbindungsvektors ***/
-    for (i = 0; i <= MAX; i++) {
-        Verbindungsvektor_halbiert[i] = 0.5*Verbindungsvektor[i];
-    }
+  /*** Berechnung des Schnittpunktes im globalen Koordinatensystem ***/
+  for (i = 0; i <= MAX; i++) {
+    Schnittpunkt[i] = K1_vec[i] + Skalar[1] * u_vec[i] + Verbindungsvektor_halbiert[i];
+  }
 
-    /*** Berechnung des Schnittpunktes im globalen Koordinatensystem ***/
-    for (i = 0; i <= MAX; i++) {
-        Schnittpunkt[i] = K1_vec[i] + Skalar[1] * u_vec[i] + Verbindungsvektor_halbiert[i];
-    }
-
-    /*** Ausgabe der beiden Skalare ***/
-    /*
-    printf("\nDie beiden Skalare sind\n\n", MAX);
-    for (i = 0; i < MAX; i++)
-        printf("\tSkalar%d = %1.2f\n", i + 1, Skalar[i]);
-    printf("\n\n");
-    printf("\nDer Verbindungsvektor ist\n\n", MAX);
-    for (i = 0; i <= MAX; i++)
-        printf("\tX%d = %1.2f\n", i + 1, Verbindungsvektor[i]);
-    printf("\n\n");
-    printf("\nDer Schnittpunkt im globalen Koordinatensystem ist\n\n", MAX);
-    for (i = 0; i <= MAX; i++)
-        printf("\tX%d = %1.2f\n", i + 1, Schnittpunkt[i]);
-    printf("\n\n");
-    */
-
-    return *Schnittpunkt;
+  position.x = Schnittpunkt[0];
+  position.y = Schnittpunkt[1];
+  position.z = Schnittpunkt[2];
 }
 
 #endif /* TRIANGULATE_H_ */
