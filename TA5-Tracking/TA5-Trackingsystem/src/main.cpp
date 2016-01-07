820d20c469903c41c7db0c15e5e3d7c1b2380d77
Mauchel1 <daniel.friedrich@haw-hamburg.de>
2016-01-07 12:03:15 +0100

    [CODE] Debugmodus und Kalibriermodus hinzugefügt

--- TA5-Tracking/TA5-Trackingsystem/src/main.cpp
+++ TA5-Tracking/TA5-Trackingsystem/src/main.cpp
@@ -8,8 +8,12 @@
 #define CAM1_FILENAME "cameraStorage1.xml"
 #define CAM2_FILENAME "cameraStorage2.xml"
 
+#define DEBUG
+#define CAMERA_CALIB_CIRCLES
+
 #include <iostream>
 #include <iomanip>
+#include <conio.h>
 using namespace std;
 
 #include "Socket.h"
@@ -32,8 +36,7 @@ using namespace cv;
 #include "triangulate.h"
 
 void printHelp() {
-  cout << "\n"
-       << "zugelassene Optionen: loadConfig, calibrateCamera, calibrate3D, setFrameMask1, setFrameMask2, save, "
+  cout << "\n" << "zugelassene Optionen: loadConfig, calibrateCamera, calibrate3D, setFrameMask1, setFrameMask2, save, "
        << "tracking, loadAndTrack, save&exit, exit" << endl;
 }
 
@@ -125,6 +128,49 @@ int main(int argc, const char** argv) {
       cin >> options;
     }
 
+#ifdef CAMERA_CALIB_CIRCLES
+    cout << "Kamerapattern wird angezeigt..." << endl;
+
+    while (1) {
+      namedWindow("Kamera 1 mit Pattern", WINDOW_AUTOSIZE);
+      namedWindow("Kamera 2 mit Pattern", WINDOW_AUTOSIZE);
+
+      VideoCapture cap1 = cam1.get_capture();
+      cap1 >> frame1;
+      VideoCapture cap2 = cam2.get_capture();
+      cap2 >> frame2;
+
+      circle(frame1, Point(frame1.cols / 2, frame1.rows / 2), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(frame2.cols / 2, frame2.rows / 2), 10, Scalar(0, 255, 0), 1);
+      circle(frame1, Point(frame1.cols / 2, frame1.rows), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(frame2.cols / 2, frame2.rows), 10, Scalar(0, 255, 0), 1);
+      circle(frame1, Point(frame1.cols, frame1.rows / 2), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(frame2.cols, frame2.rows / 2), 10, Scalar(0, 255, 0), 1);
+      circle(frame1, Point(frame1.cols, frame1.rows), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(frame2.cols, frame2.rows), 10, Scalar(0, 255, 0), 1);
+      circle(frame1, Point(0, frame1.rows), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(0, frame2.rows), 10, Scalar(0, 255, 0), 1);
+      circle(frame1, Point(frame1.cols, 0), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(frame2.cols, 0), 10, Scalar(0, 255, 0), 1);
+      circle(frame1, Point(0, frame1.rows / 2), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(0, frame2.rows / 2), 10, Scalar(0, 255, 0), 1);
+      circle(frame1, Point(frame1.cols / 2, 0), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(frame2.cols / 2, 0), 10, Scalar(0, 255, 0), 1);
+      circle(frame1, Point(0, 0), 10, Scalar(0, 255, 0), 1);
+      circle(frame2, Point(0, 0), 10, Scalar(0, 255, 0), 1);
+
+      imshow("Kamera 1 mit Pattern", frame1);
+      imshow("Kamera 2 mit Pattern", frame2);
+
+      if (waitKey(30) >= 0) {
+        break;
+      }
+    }
+
+    destroyWindow("Kamera 1 mit Pattern");
+    destroyWindow("Kamera 2 mit Pattern");
+#endif
+
     /*
      * set reference frame for tracking
      */
@@ -150,8 +196,11 @@ int main(int argc, const char** argv) {
     /*
      * main tracking routine
      */
+
+#ifdef DEBUG
     namedWindow("tracking 1", WINDOW_NORMAL);
     namedWindow("tracking 2", WINDOW_NORMAL);
+#endif
 
     while (1) {
       /*
@@ -170,10 +219,15 @@ int main(int argc, const char** argv) {
       cam1.get_newFrame(frame1);
       cam2.get_newFrame(frame2);
 
+#ifndef DEBUG
+      detect1.detectObject(frame1, pixelPos1);
+      detect2.detectObject(frame2, pixelPos2);
+#else
       if (detect1.detectObject(frame1, pixelPos1) != ERR) {
         circle(frame1, Point(pixelPos1.x, pixelPos1.y), 30, Scalar(255, 0, 0), 1);
       }
       imshow("tracking 1", frame1);
+
       if (detect2.detectObject(frame2, pixelPos2) != ERR) {
         circle(frame2, Point(pixelPos2.x, pixelPos2.y), 30, Scalar(255, 0, 0), 1);
       }
@@ -182,17 +236,14 @@ int main(int argc, const char** argv) {
       if (waitKey(30) >= 0) {
         break;
       }
+#endif
 
       /*
        * undistort pixel position
        */
       // TODO undistort
-      cout << "pixelPos1" << pixelPos1 << " pixelPos2" << pixelPos2 << endl;
-
       undistPos1 = pixelPos1; // stub
       undistPos2 = pixelPos2; // stub
-
-      cout << "undistPos1" << undistPos1 << " undistPos2" << undistPos2 << endl;
 
       /*
        * calculate 3D position - triangulate
@@ -202,8 +253,7 @@ int main(int argc, const char** argv) {
 
       triangulate(cam1.positionVector, cam1.objectVector, cam2.positionVector, cam2.objectVector, objectPos3D);
 
-      cout << "objectPos3D " << objectPos3D << endl;
-      cout << endl;
+      cout << "objectPos3D " << (int) objectPos3D.x << "\t" << (int) objectPos3D.y << "\t" << (int) objectPos3D.z << endl;
 
       /*
        * send position via UDP socket
@@ -219,6 +269,8 @@ int main(int argc, const char** argv) {
       position[6] = (int) objectPos3D.z & 0x000000FF;
 
       remoteInput.sendMessage(position, 7);
+      if (kbhit())
+        break;
     }
 
     /*
